2314494198ec0bf792f1f7f3f576cd017b019135
Neil Hickman <neil@thefreshuk.com>
2019-12-04 14:34:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,19 +4,17 @@ import os.path
 
 from setuptools import setup
 
-ROOT = os.path.dirname(__file__)
-
 setup(
     version="0.1",
     url="https://github.com/thefreshuk/aws-python-sns-message-validator",
     name="aws_sns_validator",
     description=("Validate the integrity of Amazon SNS message with support"
                  "for use with Fake_SNS"),
-    long_description=open(os.path.join(ROOT, "README.md")).read(),
+    long_description=open("README.md").read(),
     author="Neil Hickman",
     author_email="neil@thefreshuk.com",
     packages=["aws_sns_validator"],
-    package_dir={"": os.path.join(ROOT, "src")},
+    package_dir={"src":"src"},
     install_requires=[
         line.strip()
         for line in open(
